3fdbb37882de7f0c0ff305794f1cbabf536b4629
JOAO VICTOR DA SILVA GUILHERME <joao.vsguilherme@senacsp.edu.br>
2025-02-13 21:29:55 -0300

    aplicativo 1

--- sw.js
+++ sw.js
@@ -1,26 +1,36 @@
 // sw.js - Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
-      caches.open('carta-cache').then(cache => {
-        return cache.addAll([
-          './carta/index.html',
-          './carta/style.css',
-          './carta/app.js',
-          './carta/img/valete.png',
-          './carta/img/dama.png',
-          './carta/img/rei.png',
-          './carta/img/icon-192x192.png',
-          './carta/img/icon-512x512.png'
-        ]);
+      caches.open('static')
+      .then ((cache) => {
+          cache.add('./carta/index.html')
+           cache.add('./carta/style.css')
+           cache.add('./carta/app.js')
+           cache.add('./carta/img/valete.png')
+           cache.add('./carta/img/dama.png')
+           cache.add('./carta/img/rei.png')
+           cache.add('./carta/img/icon-192x192.png')
+           cache.add('./carta/img/icon-512x512.png')
+      ;
       })
     );
   });
   
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  self.addEventListener('activate',(event) => {
+    console.log("ativando o serviço worker...",event)
+    return self.clients.claim()
+})
+
+//interceptação (solicitação https servindo em cache quando off-line)
+self.addEventListener('fetch', (event) => {
+    event.respondiWitch
+        caches.match(event.request)
+        .then((response) => {
+            if(response) {
+                return response
+            } else {
+                return fetch(event.request)
+            }
+        }
+    )
+})
